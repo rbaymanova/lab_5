2f5d9c9b90014366c08bf2ea88f5c423741b2219
Ruxshona_Baymanova <r.baymanova@newuu.uz>
2025-09-24 05:30:01 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
       title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PasswordFieldDemo(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class PasswordFieldDemo extends StatefulWidget {
+  const PasswordFieldDemo({super.key});
+
+  @override
+  State<PasswordFieldDemo> createState() => _PasswordFieldDemoState();
+}
+
+class _PasswordFieldDemoState extends State<PasswordFieldDemo> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      appBar: AppBar(title: const Text('TextField Example')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your password',
+                icon: Icon(Icons.lock),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print('Password entered: ${_controller.text}');
+              },
+              child: const Text('Submit'),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class MyColumnRow extends StatelessWidget {
+  const MyColumnRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Stars Column')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Icon(Icons.star, size: 50),
+          Icon(Icons.star, size: 50),
+          Icon(Icons.star, size: 50),
+          Icon(Icons.star, size: 50),
+          Icon(Icons.star, size: 50),
+        ],
+      ),
+    );
+  }
+}
+
+class MyContainer extends StatelessWidget {
+  const MyContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(16.0),
+      height: 150,
+      width: 100,
+      decoration: BoxDecoration(
+        color: Colors.redAccent,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: const Icon(
+        Icons.home,
+        color: Colors.white,
+        size: 40,
       ),
     );
   }
